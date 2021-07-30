83f36fb63d413d97926a125e69e3d1de24fe2390
Fernando Serra <fernando.serra.r@gmail.com>
2021-07-30 17:57:01 -0400

    Jetpack Compose

---
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.jetpackcompose
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
+import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            //JetpackComposeTheme {
+            AppTheme {
+                // A surface container using the 'background' color from the theme
+                //Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                    Divider(color = Color.Black)
+                    Greeting("Android2")
+                    Divider(color = Color.Black)
+                    Greeting("Android3")
+                    Divider(color = Color.Black)
+                    ButtonToast()
+                    Divider(color = Color.Black)
+                    ButtonRedirect()
+                //}
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", Modifier.padding(24.dp))
+
+}
+
+@Preview(name="Default",showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeTheme {
+        Greeting("Android")
+        Msj()
+    }
+}
+
+/*Nombres de  funciones Composable es preferible
+  colocarla en mayuscula para no confundirlas con
+  el nombre de una funcion normalmente*/
+@Composable
+fun Msj(){
+    val size=55.sp
+    AppTheme() {
+        Text(text = "Hola 22", fontSize = size)
+    }
+
+}
+
+@Composable
+fun ButtonToast(){
+    val context = LocalContext.current
+    Button(onClick = {
+       Toast.makeText(context, "You just clicked a Clickable", Toast.LENGTH_LONG)
+            .show()
+    }) {
+        Text(text = "Toas")
+    }
+}
+
+@Composable
+fun ButtonRedirect(){
+    val context = LocalContext.current
+    Button(onClick = {
+        context.startActivity(Intent(
+            context,
+            ListActivity::class.java
+        ))
+    }) {
+        Text(text = "Redireccionar")
+    }
+}
+
+@Composable
+fun AppTheme(content:@Composable() () ->Unit){
+    JetpackComposeTheme{
+        Surface(color= Color.Cyan) {
+            Column() {
+                content()
+            }
+
+        }
+    }
+}
