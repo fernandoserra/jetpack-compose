3743b5bcffccbfb6268a5c5c8c1402659cbf4069
fernandoserra <fernando.serra.r@gmail.com>
2021-08-23 23:26:24 -0400

    Update Fundation

---
+++ app/src/main/java/com/example/jetpackcompose/foundation/Foundation.kt
@@ -0,0 +1,42 @@
+package com.example.jetpackcompose.foundation
+
+import androidx.compose.runtime.Composable
+
+/**
+ * Link examples: https://foso.github.io/Jetpack-Compose-Playground/#foundation
+ * */
+
+@Composable
+fun Canvas(){
+
+}
+
+@Composable
+fun Image(){
+
+}
+
+@Composable
+fun LazyColumn(){
+
+}
+
+@Composable
+fun LazyRow(){
+
+}
+
+@Composable
+fun LazyVerticalGrid(){
+
+}
+
+@Composable
+fun Shape(){
+
+}
+
+@Composable
+fun Text(){
+
+}
