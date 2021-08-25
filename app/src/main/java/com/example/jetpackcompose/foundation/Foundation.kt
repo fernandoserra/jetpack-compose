ae8308019067d82996d382ba42e5a592bf919fcb
fernandoserra <fernando.serra.r@gmail.com>
2021-08-25 00:14:59 -0400

    Update Fundation  fun ShapeDemo

--- app/src/main/java/com/example/jetpackcompose/foundation/Foundation.kt
+++ app/src/main/java/com/example/jetpackcompose/foundation/Foundation.kt
@@ -1,9 +1,20 @@
 package com.example.jetpackcompose.foundation
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -11,6 +22,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.R
 
 /**
@@ -51,8 +63,39 @@ fun LazyVerticalGridDemo(){
 
 @Composable
 fun ShapeDemo(){
+    Column{
+        TypeShape(shape = RectangleShape)
+        Spacer(modifier = Modifier.height(10.dp))
+        TypeShape(shape = CircleShape)
+        Spacer(modifier = Modifier.height(10.dp))
+        TypeShape(shape = RoundedCornerShape(10.dp))
+        Spacer(modifier = Modifier.height(10.dp))
+        TypeShape(shape = CutCornerShape(10.dp))
+    }
 
 }
+
+@Composable
+fun TypeShape(shape: Shape){
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentSize(Alignment.Center)) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .clip(shape)
+                .background(Color.Red)
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewShapeDeo(){
+    ShapeDemo()
+}
+
 
 @Composable
 fun TextDemo(){
