33eef0c2ce23daccb29a6b5656835a5abd581a00
fernandoserra <fernando.serra.r@gmail.com>
2021-08-24 23:59:27 -0400

    Update Layouts

--- app/src/main/java/com/example/jetpackcompose/foundation/Layouts.kt
+++ app/src/main/java/com/example/jetpackcompose/foundation/Layouts.kt
@@ -1,2 +1,38 @@
 package com.example.jetpackcompose.foundation
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BoxDemo(){
+
+}
+
+
+@Composable
+fun BoxWithConstraintsDemo(){
+
+}
+
+
+@Composable
+fun ColumnDemo(){
+
+}
+
+
+@Composable
+fun ConstraintLayoutDemo(){
+
+}
+
+
+@Composable
+fun RowDemo(){
+
+}
+
+
+@Composable
+fun SpacerDemo(){
+
+}
