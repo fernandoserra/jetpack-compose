b5485818306085d96da3629367d70c9ec3251103
fernandoserra <fernando.serra.r@gmail.com>
2021-08-31 22:53:51 -0400

    update Layout

--- app/src/main/java/com/example/jetpackcompose/foundation/Layouts.kt
+++ app/src/main/java/com/example/jetpackcompose/foundation/Layouts.kt
@@ -2,17 +2,14 @@ package com.example.jetpackcompose.foundation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.jetpackcompose.R
 
 
 /**
