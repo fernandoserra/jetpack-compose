6df5a2e34b9b9f58363dd8b82919474ea33efa8f
fernandoserra <fernando.serra.r@gmail.com>
2021-09-06 22:30:21 -0300

    Update MainActivity

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
@@ -20,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.accompanist.Pager
 import com.example.jetpackcompose.accompanist.Pager2
+import com.example.jetpackcompose.demo.MainContent
 import com.example.jetpackcompose.ui.theme.HorizontalScroll
 import com.example.jetpackcompose.ui.theme.HorizontalScrollImagen
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -27,15 +29,19 @@ import com.google.accompanist.pager.ExperimentalPagerApi
 
 class MainActivity : ComponentActivity() {
 
+    @ExperimentalAnimationApi
     @ExperimentalPagerApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            MainContent()
+
             //JetpackComposeTheme {
             //AppTheme {
                 // A surface container using the 'background' color from the theme
                 //Surface(color = MaterialTheme.colors.background) {
-                    Column {
+                    /*Column {
                         /*Greeting("Android")
                         Divider(color = Color.Black)
                         Greeting("Android2")
@@ -57,7 +63,7 @@ class MainActivity : ComponentActivity() {
                         Divider(color = Color.Black)
                         ButtonRedirect()
 
-                     }
+                     }*/
                 //}
            // }
         }
--- app/src/main/java/com/example/jetpackcompose/demo/MutableStateListOf.kt
+++ app/src/main/java/com/example/jetpackcompose/demo/MutableStateListOf.kt
@@ -1,2 +1,113 @@
 package com.example.jetpackcompose.demo
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.util.*
+
+
+@ExperimentalAnimationApi
+@Composable
+fun MainContent(){
+    Box(
+        Modifier
+            .background(Color(0xFFEDEAE0))
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        val uuids = remember { mutableStateListOf<String>() }
+
+        LazyColumn(
+            Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            itemsIndexed(uuids) { index, item ->
+                val visibility by remember {
+                    mutableStateOf(
+                        uuids.contains(item) && item == uuids[index]
+                    )
+                }
+
+                AnimatedVisibility(
+                    visible = visibility
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(
+                                align = Alignment.CenterVertically
+                            ),
+                        elevation = 4.dp,
+                        shape = RoundedCornerShape(8.dp),
+                        backgroundColor = if (index % 2 == 0)
+                            Color(0xFF9C27B0) else Color(0xFF673AB7)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(12.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Text(
+                                text = item.take(5).capitalize(),
+                                modifier = Modifier.padding(
+                                    start = 12.dp, top = 12.dp,
+                                    end = 12.dp, bottom = 4.dp
+                                ),
+                                textAlign = TextAlign.Center,
+                                fontSize = 30.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White
+                            )
+                            TextButton(
+                                onClick = { uuids.remove(item) },
+                                colors = ButtonDefaults.textButtonColors(
+                                    backgroundColor = Color(0xFF960018),
+                                    contentColor = Color.White
+                                )
+                            ) {
+                                Text(text = "Remove")
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+        FloatingActionButton(
+            onClick = {
+                UUID.randomUUID().toString().apply {
+                    uuids.add(this)
+                }
+            },
+            Modifier.align(Alignment.BottomEnd),
+            backgroundColor = Color(0xFFFE4164)
+        ) { Icon(Icons.Filled.Add,"") }
+    }
+}
+
+
+@ExperimentalAnimationApi
+@Preview
+@Composable
+fun PreviewComposable(){
+    MainContent()
+}
