6ed291500905277a3ee61a859934e29d8ed19c93
fernandoserra <fernando.serra.r@gmail.com>
2021-10-01 20:34:58 -0300

    Update AlertDialogDemo()

--- app/src/main/java/com/example/jetpackcompose/material/Material.kt
+++ app/src/main/java/com/example/jetpackcompose/material/Material.kt
@@ -21,7 +21,53 @@ import com.example.jetpackcompose.data.Datos
 
 @Composable
 fun AlertDialogDemo(){
+    MaterialTheme {
+        Column {
+            val openDialog = remember { mutableStateOf(false)  }
 
+            Button(onClick = {
+                openDialog.value = true
+            }) {
+                Text("Click me")
+            }
+
+            if (openDialog.value) {
+
+                AlertDialog(
+                    onDismissRequest = {
+                        // Dismiss the dialog when the user clicks outside the dialog or on the back
+                        // button. If you want to disable that functionality, simply use an empty
+                        // onCloseRequest.
+                        openDialog.value = false
+                    },
+                    title = {
+                        Text(text = "Dialog Title")
+                    },
+                    text = {
+                        Text("Here is a text ")
+                    },
+                    confirmButton = {
+                        Button(
+
+                            onClick = {
+                                openDialog.value = false
+                            }) {
+                            Text("This is the Confirm Button")
+                        }
+                    },
+                    dismissButton = {
+                        Button(
+
+                            onClick = {
+                                openDialog.value = false
+                            }) {
+                            Text("This is the dismiss Button")
+                        }
+                    }
+                )
+            }
+        }
+    }
 }
 
 @Composable
