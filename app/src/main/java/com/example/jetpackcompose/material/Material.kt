0c739e7e76710cc5109bd8635e788a4b24cbe3d1
fernandoserra <fernando.serra.r@gmail.com>
2021-10-06 23:32:10 -0300

    Update SlideDemo

--- app/src/main/java/com/example/jetpackcompose/material/Material.kt
+++ app/src/main/java/com/example/jetpackcompose/material/Material.kt
@@ -9,10 +9,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
@@ -21,7 +18,6 @@ import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.R
 import com.example.jetpackcompose.data.Datos
 import kotlinx.coroutines.launch
-
 
 @Composable
 fun AlertDialogDemo(){
@@ -156,7 +152,6 @@ fun CircularProgressIndicatorDemo(){
 fun PreviewCircularProgressIndicatedDemo(){
     CircularProgressIndicatorDemo()
 }
-
 
 @Composable
 fun DropdownMenuDemo(){
@@ -301,7 +296,9 @@ fun ScaffoldDemo(){
 
 @Composable
 fun SliderDemo(){
-
+    var sliderPosition by remember { mutableStateOf(0f) }
+    Text(text = sliderPosition.toString())
+    Slider(value = sliderPosition, onValueChange = { sliderPosition = it })
 }
 
 @Composable
