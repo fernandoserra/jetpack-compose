e4e9873b10e245280933f5764cba54c2c58360f4
fernandoserra <fernando.serra.r@gmail.com>
2021-10-10 22:17:50 -0300

    Update TopAppBarDemo()

--- app/src/main/java/com/example/jetpackcompose/material/Material.kt
+++ app/src/main/java/com/example/jetpackcompose/material/Material.kt
@@ -8,7 +8,10 @@ import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -379,5 +382,27 @@ fun PreviewTextFieldDemo(){
 
 @Composable
 fun TopAppBarDemo(){
+    Column {
+        TopAppBar(
+            elevation = 4.dp,
+            title = {
+                Text("I'm a TopAppBar")
+            },
+            backgroundColor =  MaterialTheme.colors.primarySurface,
+            navigationIcon = {
+                IconButton(onClick = {/* Do Something*/ }) {
+                    Icon(Icons.Filled.ArrowBack, null)
+                }
+            }, actions = {
+                IconButton(onClick = {/* Do Something*/ }) {
+                    Icon(Icons.Filled.Share, null)
+                }
+                IconButton(onClick = {/* Do Something*/ }) {
+                    Icon(Icons.Filled.Settings, null)
+                }
+            })
 
+        Text("Hello World")
+
+    }
 }
