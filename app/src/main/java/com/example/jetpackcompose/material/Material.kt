248a0c869d2f49dcf1d3cebdb40f91713ba60e4c
fernandoserra <fernando.serra.r@gmail.com>
2021-10-17 20:07:55 -0300

    Update ScaffolDemo

--- app/src/main/java/com/example/jetpackcompose/material/Material.kt
+++ app/src/main/java/com/example/jetpackcompose/material/Material.kt
@@ -293,7 +293,19 @@ fun RadioButtonDemoPreview(){
 
 @Composable
 fun ScaffoldDemo(){
-
+    val materialBlue700= Color(0xFF1976D2)
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Open))
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { TopAppBar(title = {Text("TopAppBar")},backgroundColor = materialBlue700)  },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = { FloatingActionButton(onClick = {}){
+            Text("X")
+        } },
+        drawerContent = { Text(text = "drawerContent") },
+        content = { Text("BodyContent") },
+        bottomBar = { BottomAppBar(backgroundColor = materialBlue700) { Text("BottomAppBar") } }
+    )
 }
 
 @Composable
